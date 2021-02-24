8820b9a6bf4e574390406f687ad884d2826bf197
alx787 <kaanova@gmail.com>
2021-02-24 16:09:41 +0300

    create login fragment

--- app/src/main/java/ru/alx/waybills/MainActivity.kt
+++ app/src/main/java/ru/alx/waybills/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.alx.waybills
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.alx.waybills.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = LoginFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
     }
 }
--- app/src/main/java/ru/alx/waybills/fragments/LoginFragment.kt
+++ app/src/main/java/ru/alx/waybills/fragments/LoginFragment.kt
@@ -5,23 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import ru.alx.waybills.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
+private const val TAG = "LoginFragment"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-    // тест
+
+    private lateinit var editTextTextUserName: EditText
+    private lateinit var editTextTextPassword: EditText
+    private lateinit var buttonLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,27 +30,21 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        editTextTextUserName = view.findViewById(R.id.editTextTextUserName) as EditText
+        editTextTextPassword = view.findViewById(R.id.editTextTextPassword) as EditText
+        buttonLogin = view.findViewById(R.id.buttonLogin) as Button
+
+        return view
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment LoginFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            LoginFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    override fun onStart() {
+        super.onStart()
+
+        buttonLogin.setOnClickListener { view ->
+            Toast.makeText(requireContext(), "Login pressed", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
